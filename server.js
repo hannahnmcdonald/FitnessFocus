c76c47d38bc892bef01289fb8e8e6fcca68c7e85
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-10-28 16:24:08 -0400

    further heroku troubleshooting, verfifying db name is same

--- server.js
+++ server.js
@@ -18,21 +18,21 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+// Connects either to MongoDB Atlas if on heroku site or to the local, if running locally
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  },
-);
+});
 
 // routes
 app.use(htmlRoute);
 app.use(apiRoute);
 
-// Listening
+// Listening- Starting the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
